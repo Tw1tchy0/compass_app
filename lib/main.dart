9ac5fb02e593f8bd5a5b43d401177333b42ef089
Tw1tchy0 <157633691+Tw1tchy0@users.noreply.github.com>
2024-02-28 15:37:11 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
@@ -14,52 +15,102 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CircleMarkersDemo extends StatelessWidget {
-  final double circleRadius = 100; // radius of the circle
-  final int numOfMarkers = 36; // number of markers (every 10 degrees)
-  final double targetLat = 40.9254; // Your latitude
-  final double targetLong = 73.9629; // Your longitude
-  final double myLat = 40.5008; // Target latitude
-  final double myLong = 74.4474; // Target longitude
+class CircleMarkersDemo extends StatefulWidget {
+  @override
+  _CircleMarkersDemoState createState() => _CircleMarkersDemoState();
+}
+
+class _CircleMarkersDemoState extends State<CircleMarkersDemo> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Compass App Testing'),
+        title: Text('Compass App'),
       ),
-      body: Center(
-        child: CustomPaint(
-          size: const Size(300, 300), // adjust size as needed
-          painter: CircleMarkersPainter(
-            circleRadius: circleRadius,
-            numOfMarkers: numOfMarkers,
-            targetLat: targetLat,
-            targetLong: targetLong,
-            myLat: myLat,
-            myLong: myLong,
+      body: _selectedIndex == 0 ? MarkerView() : BlankPageView(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Compass View',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.square_foot),
+            label: 'Contacts View',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+// Compass View
+class MarkerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double circleRadius = 100; // radius of the circle
+    final int numOfMarkers = 36; // number of markers (every 10 degrees)
+    final double myLatitude = 40.5008; // Your latitude
+    final double myLongitude =  74.4474; // Your longitude
+    List<TargetMarker> targetMarkers = [
+      TargetMarker(LatLng(48.8566, 2.3522), Colors.red), // Paris
+      TargetMarker(LatLng(36.7783, 119.4179), Colors.green), // Cali
+      TargetMarker(LatLng(82.8628, 135.0000), Colors.blue), // Antarc
+    ];
+
+    return Container(
+      color: Colors.grey[900], // Set the background color here
+      alignment: Alignment.center, // Center the child
+      child: CustomPaint(
+        size: const Size(300, 300), // adjust size as needed
+        painter: CircleMarkersPainter(
+          circleRadius: circleRadius,
+          numOfMarkers: numOfMarkers,
+          myLat: myLatitude,
+          myLong: myLongitude,
+          targetMarkers: targetMarkers,
         ),
       ),
     );
   }
 }
 
+class BlankPageView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Blank Page View'),
+    );
+  }
+}
+
+
+
+
 class CircleMarkersPainter extends CustomPainter {
   final double circleRadius;
   final int numOfMarkers;
-  final double targetLat;
-  final double targetLong;
   final double myLat;
   final double myLong;
+  final List<TargetMarker> targetMarkers;
 
   CircleMarkersPainter({
-    required this.circleRadius, 
-    required this.numOfMarkers, 
-    required this.targetLat,
-    required this.targetLong,
+    required this.circleRadius,
+    required this.numOfMarkers,
     required this.myLat,
     required this.myLong,
-    });
+    required this.targetMarkers,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -67,29 +118,41 @@ class CircleMarkersPainter extends CustomPainter {
 
     // Draw circle
     final paint = Paint()
-      ..color = Colors.blue
+      ..color = Colors.grey
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 2.0;
+      ..strokeWidth = 3.0;
     canvas.drawCircle(center, circleRadius, paint);
 
-    // Calculate relative angle
-    final myX = targetLong;
-    final myY = targetLat;
-    final targetX = myLong;
-    final targetY = myLat;
-    final dx = targetX - myX;
-    final dy = targetY - myY;
-    final angle = math.atan2(dy, dx);
-
-    // Draw markers
-    final markerPaint = Paint()..color = Colors.red;
-    final markerX = center.dx + circleRadius * math.cos(angle);
-    final markerY = center.dy + circleRadius * math.sin(angle);
-    canvas.drawCircle(Offset(markerX, markerY), 4, markerPaint);
+    // Draw markers for each target coordinate
+    for (final marker in targetMarkers) {
+      final targetCoord = marker.coordinate;
+      final color = marker.color;
+      final dx = targetCoord.longitude - myLong;
+      final dy = targetCoord.latitude - myLat;
+      final angle = math.atan2(dy, dx);
+      final markerPaint = Paint()..color = color;
+      final markerX = center.dx + circleRadius * math.cos(angle);
+      final markerY = center.dy + circleRadius * math.sin(angle);
+      canvas.drawCircle(Offset(markerX, markerY), 7, markerPaint);  // # is size of the markers
+    }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
+}
+
+class LatLng {
+  final double latitude;
+  final double longitude;
+
+  LatLng(this.latitude, this.longitude);
+}
+
+class TargetMarker {
+  final LatLng coordinate;
+  final Color color;
+
+  TargetMarker(this.coordinate, this.color);
 }
